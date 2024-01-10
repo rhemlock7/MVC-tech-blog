3bc1625a05afa8b33b29af3eae6c2de3bb966924
Ryan Hemlock <ryan@ryanhemlock.com>
2024-01-10 18:05:47 -0500

    Login functionality now working

--- models/index.js
+++ models/index.js
@@ -7,21 +7,18 @@ User.hasMany(Blog, {
     onDelete: 'CASCADE'
 });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
-
 Blog.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-Comment.belongsTo(Blog, {
-    foreignKey: 'blog_id'
+Blog.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
 });
+
 
 module.exports = { User, Blog, Comment };
