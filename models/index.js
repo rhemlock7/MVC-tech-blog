2908912ff8cda084c1fa512f511f913b4ddbf59f
Ryan Hemlock <ryan@ryanhemlock.com>
2024-01-11 15:55:21 -0500

    Can now view comments and blog posts

--- models/index.js
+++ models/index.js
@@ -8,16 +8,18 @@ User.hasMany(Blog, {
 });
 
 Blog.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-Blog.hasMany(Comment, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 
